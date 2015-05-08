d6736a2ba9d2286da43c2585cea527f988d84d76
Boris <BorisLaptop@Boris.Boris>
2015-05-08 14:44:20 +0200

    Beute Manipulator und Tests erstellt

---
+++ Source/src/controller/BeuteManipulator.java
@@ -0,0 +1,210 @@
+package controller;
+
+import java.util.List;
+
+import javax.persistence.EntityExistsException;
+import javax.persistence.EntityManager;
+import javax.persistence.LockTimeoutException;
+import javax.persistence.NoResultException;
+import javax.persistence.NonUniqueResultException;
+import javax.persistence.PersistenceException;
+import javax.persistence.PessimisticLockException;
+import javax.persistence.QueryTimeoutException;
+import javax.persistence.RollbackException;
+import javax.persistence.TransactionRequiredException;
+
+
+
+import javax.persistence.TypedQuery;
+
+import org.hibernate.HibernateException;
+
+import controller.interfaces.DBManipulator;
+import model.Ausruestung;
+import model.Beute;
+import model.Gegner;
+import model.Spieler;
+import model.interfaces.DBObject;
+
+public class BeuteManipulator implements DBManipulator{
+    private static EntityManager theManager = EntityManagerFactoryProvider.getFactory().createEntityManager();
+    private static BeuteManipulator singelton;
+    
+    private BeuteManipulator() {}
+    
+    @Override
+    public boolean add(DBObject entity) {
+        theManager.getTransaction().begin();
+        try {
+            theManager.persist((Beute) entity);
+        }
+        catch (EntityExistsException addExceptionOne) {
+            System.err.println("EntityExistsException: "
+                    + addExceptionOne.getMessage());
+            theManager.getTransaction().commit();
+            return false;
+        }
+        catch (TransactionRequiredException addExceptionTwo) {
+            System.err.println("TransactionRequiredException: "
+                    + addExceptionTwo.getMessage());
+            return false;
+        }
+        catch (IllegalArgumentException addExceptionThree) {
+            System.err.println("IllegalArgumentException: "
+                    + addExceptionThree.getMessage());
+            return false;
+        }
+        
+        theManager.getTransaction().commit();
+        return true;
+    }
+    
+    @Override
+    public boolean delete(DBObject entity) {
+        boolean returnValue = true;
+        try {
+            Beute defaultBeute = getDefaultBeute();
+            theManager.getTransaction().begin();
+            replaceBeute(defaultBeute, (Beute)entity);
+            theManager.remove((Beute) entity);
+        }
+        catch (TransactionRequiredException persistExceptionTwo) {
+            System.err.println("TransactionRequiredException: "
+                    + persistExceptionTwo.getMessage());
+            returnValue = false;
+        }
+        catch (IllegalArgumentException persistExceptionThree) {
+            System.err.println("IllegalArgumentException in delete from BeuteManipulator"
+                    + persistExceptionThree.getMessage());
+            returnValue = false;
+        }
+        catch (PersistenceException persistExceptionFinal) {
+            System.err.println("PersistenceException: "
+                    + persistExceptionFinal.getMessage());
+            returnValue = false;
+        }
+        finally {
+            try {
+                theManager.getTransaction().commit();
+            }
+            catch (RollbackException commitExceptionOne) {
+                System.err.println("RollBackException: "
+                        + commitExceptionOne.getMessage());
+                return false;
+            }
+            catch (PersistenceException commitExceptionTwo) {
+                System.err.println("PersistenceException: "
+                        + commitExceptionTwo.getMessage());
+                theManager.getTransaction().commit();
+                return false;
+            }
+        }
+        
+        return returnValue;
+    }
+    
+    
+    
+    private boolean replaceBeute(Beute defaultBeute, Beute deletedBeute) {
+        TypedQuery<Gegner> getAllRows;
+        try {
+            getAllRows = theManager.createQuery("UPDATE Gegner SET beuteTyp_ = "+ defaultBeute.ID_ 
+                    + " WHERE beuteTyp_ = " + deletedBeute.ID_, Gegner.class);
+        }
+        catch(IllegalArgumentException createQueryExceptionOne) {
+            System.err.println("IllegalArgumentException: in replace Beute from BeuteManipulator ");
+            return false;
+        }
+        try {
+            System.out.println("Hallo");
+            getAllRows.executeUpdate();
+            return true;
+        }
+        catch(HibernateException e) {
+            return false;
+        }
+    }
+    
+    
+    private Beute getDefaultBeute() {
+        TypedQuery<Beute> getAllRows;
+        theManager.getTransaction().begin();
+        try {
+            getAllRows = theManager.createQuery("FROM Beute WHERE profil_ = '" + Beute.DEFAULTNAME + "'",
+                Beute.class);
+        }
+        catch(IllegalArgumentException createQueryExceptionOne)   {
+            System.err.println("IllegalArgumentException: ");
+            return null;
+        }
+        try {
+            return getAllRows.getSingleResult();
+        }
+        catch(NoResultException noResult) {
+            Beute defaultBeute = new Beute();
+            add(defaultBeute);
+            return getAllRows.getSingleResult();
+        }
+        catch(NonUniqueResultException notUnique) {
+            // TODO:: Hier sollte evtl. noch was gemacht werden.
+            System.err.println("There is more than one Beute: ");
+            return getAllRows.getResultList().get(0);
+        }
+        catch(QueryTimeoutException getResultListExceptionTwo)  {
+            System.err.println("QueryTimeoutException: ");
+            return null;
+        }
+        catch(TransactionRequiredException getResultListExceptionThree)  {
+            System.err.println("TransactionRequiredException: ");
+            return null;
+        }
+        catch(PessimisticLockException getResultListExceptionFour)  {
+            System.err.println("PessimisticLockException: ");
+            return null;
+        }
+        catch(LockTimeoutException getResultListExceptionFive)  {
+            System.err.println("LockTimeoutException: ");
+            return null;
+        }
+        catch(PersistenceException getResultListExceptionSix)  {
+            System.err.println("PersistenceException: ");
+            return null;
+        }
+        finally {
+            try {
+                theManager.getTransaction().commit();
+            }
+            catch (RollbackException commitExceptionOne) {
+                System.err.println("RollBackException: "
+                        + commitExceptionOne.getMessage());
+                return false;
+            }
+            catch (PersistenceException commitExceptionTwo) {
+                System.err.println("PersistenceException: "
+                        + commitExceptionTwo.getMessage());
+                theManager.getTransaction().commit();
+                return false;
+            }
+        }
+    }
+    
+    
+    
+    @Override
+    public boolean update(DBObject entity) {
+        // TODO Auto-generated method stub
+        return false;
+    }
+    
+    public List<DBObject> getAll() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+    
+    public static BeuteManipulator getInstance() {
+        if (singelton == null) {
+            singelton = new BeuteManipulator();
+        }
+        return singelton;
+    }
+}
---
+++ Source/src/tests/controller/BeuteManipulatorTest.java
@@ -0,0 +1,81 @@
+package tests.controller;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertSame;
+
+import javax.persistence.EntityManager;
+
+import model.Beute;
+
+import org.junit.BeforeClass;
+import org.junit.Ignore;
+import org.junit.Test;
+
+import controller.BeuteManipulator;
+import controller.EntityManagerFactoryProvider;
+
+public class BeuteManipulatorTest {
+    
+    private static BeuteManipulator testInstance;
+    private static Beute testBeute;
+    
+    private static EntityManager theManager;
+
+    @BeforeClass
+    public static void setUpBeforeClass() throws Exception {
+        testInstance = BeuteManipulator.getInstance();
+        testBeute = new Beute();
+        theManager = EntityManagerFactoryProvider.getFactory()
+                .createEntityManager();
+    }
+
+    @Test
+    public void getInstanceTest() {
+        testInstance = BeuteManipulator.getInstance();
+        assertNotNull(testInstance);
+    }
+
+    @Test
+    public void twoInstanceAreSame() {
+        BeuteManipulator testInstanceTwo = BeuteManipulator
+                .getInstance();
+        assertSame(testInstanceTwo, testInstance);
+    }
+
+    @Test
+    public void testAdd() {
+        assertTrue(testInstance.add(testBeute));
+        
+    }
+    
+    @Test
+    public void testNullAdd() {
+        assertFalse(testInstance.add(null));
+
+    }
+    
+    @Ignore
+    @Test
+    public void twoBeuteAddAreSame() {
+       testInstance.add(testBeute);
+       theManager.detach(testBeute);
+       assertFalse(testInstance.add(testBeute));
+    }
+    
+    @Test
+    public void testDelete() {
+//        assertNull(testBeute);
+        assertTrue(testInstance.delete(testBeute));
+    }
+
+    @Ignore
+    @Test
+    public void twoBeuteDeleteAreSame() {
+        assertFalse(testInstance.delete(testBeute));
+    }
+    
+}
