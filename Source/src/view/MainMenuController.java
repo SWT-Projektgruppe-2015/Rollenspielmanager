3f2c59498dfae2758151ec22ad98cb3932457500
Britta Heymann <britta_hey@web.de>
2015-05-15 09:31:20 +0200

    Gruppe kann im Mainmenu angezeigt werden.

--- Source/src/view/MainMenuController.java
+++ Source/src/view/MainMenuController.java
@@ -1,12 +1,29 @@
 package view;
 
+import model.Gegner;
+import model.Gruppe;
+import model.Spieler;
 import controller.Hauptprogramm;
+import controller.manipulators.GruppenManipulator;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.fxml.FXML;
 import javafx.scene.control.ComboBox;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 
 public class MainMenuController {
     @FXML
-    private ComboBox<String> gruppenDropDown_;
+    private ComboBox<Gruppe> gruppenDropDown_;
+    @FXML
+    private TableView<Spieler> spielerInGruppe_;
+    @FXML
+    private TableColumn<Spieler, String> name_;
+    @FXML
+    private TableColumn<Spieler, Integer> kreis_;
+    @FXML
+    private TableColumn<Spieler, Integer> level_;
     
     private Hauptprogramm hauptProgramm_;
     
@@ -17,8 +34,26 @@ public class MainMenuController {
     
     @FXML
     private void initialize() {
-        gruppenDropDown_.getItems().addAll("Keine Gruppe ausgewählt", "Test");
-        gruppenDropDown_.setValue("Keine Gruppe ausgewählt");
+        gruppenDropDown_.getItems().setAll(GruppenManipulator.getInstance().getAll());
+        gruppenDropDown_.getSelectionModel().selectedItemProperty()
+        .addListener(new ChangeListener<Gruppe>() {
+            @Override
+            public void changed(
+                    ObservableValue<? extends Gruppe> observable,
+                    Gruppe oldValue, Gruppe newValue) {
+                updateGruppenTableView(newValue);
+            }
+        });
+        
+        kreis_.setCellValueFactory(new PropertyValueFactory<Spieler, Integer>("kreis_"));
+        level_.setCellValueFactory(new PropertyValueFactory<Spieler, Integer> ("level_"));
+        name_.setCellValueFactory(new PropertyValueFactory<Spieler, String>("name_"));
+    }
+
+    
+    
+    private void updateGruppenTableView(Gruppe newValue) {
+        spielerInGruppe_.getItems().setAll(newValue.getAllSpieler());
     }
     
     
