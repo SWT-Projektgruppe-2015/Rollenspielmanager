b4765af5eb6091e74f8f579c9f47208702967d59
Boris <BorisLaptop@Boris.Boris>
2015-06-26 16:22:36 +0200

    Kampfende belegt Felder mit Attributen

--- Source/src/view/controller/KampfendeController.java
+++ Source/src/view/controller/KampfendeController.java
@@ -1,5 +1,53 @@
 package view.controller;
 
+import java.util.List;
+
+import view.tabledata.ExpCategory;
+import javafx.fxml.FXML;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import model.Gegenstand;
+import model.GegnerEinheit;
+import model.Spieler;
+
 public class KampfendeController {
+    @FXML
+    private TableView<GegnerEinheit> gegnerTableView_;
+    @FXML
+    private TableColumn<GegnerEinheit, String> gegnerColumn_;
+    
+    @FXML
+    private TableView<ExpCategory> expTableView_;
+    @FXML
+    private TableColumn<ExpCategory, String> spielereinflussColumn_;
+    @FXML
+    private TableColumn<ExpCategory, Integer> expColumn_;
+    
+    @FXML
+    private TableView<Gegenstand> beuteTableView_;
+    @FXML
+    private TableColumn<Gegenstand, String> beuteColumn_;
+    @FXML
+    private TableColumn<Gegenstand, Integer> traglastColumn_;
+    
+    @FXML
+    private TextField beuteWertTextField_;
+    @FXML
+    private TextField beuteKategorieTextField_;
+    
+    @FXML
+    private TextField inventarWertTextField_;
+    @FXML
+    private TextField inventarStreuungTextField_;
+    @FXML
+    private TextField ausruestungMalusTextField_;
+    @FXML
+    private TextField ausruestungStreuungTextField_;
+    
+    
+    public void initialize(List<Spieler> allSpieler) {
+        
+    }
     
 }
