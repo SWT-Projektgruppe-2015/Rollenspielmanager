b3ac4ae556b2653e2966139943ed3c59d4f9f250
Boris <BorisLaptop@Boris.Boris>
2015-06-26 16:23:18 +0200

    Vergessen zu pushen. Gehoert zu KampfendeController

---
+++ Source/src/view/tabledata/ExpCategory.java
@@ -0,0 +1,35 @@
+package view.tabledata;
+
+import model.Spieler;
+import javafx.beans.property.IntegerProperty;
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+
+public class ExpCategory {
+    private StringProperty name_;
+
+    private IntegerProperty exp_;
+    
+    public ExpCategory(Spieler spieler, int exp) {
+        name_ = new SimpleStringProperty();
+        exp_ = new SimpleIntegerProperty();
+        if(spieler == null) {
+            name_.setValue("normal");
+            exp_.setValue(exp);
+        }
+        else{
+            name_.setValue(spieler.getName_());
+            exp_.setValue(exp * spieler.getExpFactor());
+        }
+    }
+
+    public StringProperty getName_() {
+        return name_;
+    }
+    
+    public IntegerProperty getExp_() {
+        return exp_;
+    }
+    
+}
