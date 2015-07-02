9fe5b73215d00054c3513eef25227fd9370e35a7
Andreas Kofer <yipiyip3@yahoo.com>
2015-07-03 01:33:22 +0200

    Der Text der von den Buttons ueberlapt wird ist jetzt nach rechts verschoben, aber bie der tableview ist es nur fuer namen bis zu einer laenge, da dort nur der text zentriert anstatt padding da ich nicht auf die tablecell von genau der column gekommen bin und das padding in der column selber zu machen verschiebt auch den header.

--- Source/src/view/controller/TeilnehmerAuswahlController.java
+++ Source/src/view/controller/TeilnehmerAuswahlController.java
@@ -16,6 +16,7 @@ import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
+import javafx.geometry.Insets;
 import javafx.scene.control.CheckBox;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.ListCell;
